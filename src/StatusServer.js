1e58ce661537d8c2e2669fcb83397b77970bc0f1
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-08-15 19:30:56 +0300

    fix termination error

--- src/StatusServer.js
+++ src/StatusServer.js
@@ -1,5 +1,5 @@
 const { Client, ActivityType, OAuth2Scopes, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
-const { Server } = require('@fabricio-191/valve-server-query');
+const { queryGameServerInfo, queryGameServerPlayer } = require('steam-server-query');
 
 const hasId = (input) => /^\d+$/.test(input);
 
@@ -14,7 +14,7 @@ class StatusServer {
     constructor(serverData, config) {
         this.serverData = serverData;
         this.config = config;
-        this.server = {};
+        this.host = serverData.host;
         this.bot = {};
         this.message = {};
         this.online = 0;
@@ -24,15 +24,10 @@ class StatusServer {
     * Initializing server
     */
     async init() {
-        const { token, host, name } = this.serverData;
-        const { channelId, timeout_ms, update_ms, startMsg } = this.config;
+        const { token, name } = this.serverData;
+        const { channelId, update_ms, startMsg } = this.config;
 
         if (token.length < 10) return;
-
-        const ip = host.split(':')[0];
-        const port = Number(host.split(':')[1]);
-        const timeout = Number(timeout_ms);
-        this.server = await Server({ ip, port, timeout });
 
         const client = new Client({ intents: [] });
         await client.login(token);
@@ -76,32 +71,33 @@ class StatusServer {
     * Updating server info
     */
     async update() {
-        const { maps, statusMsg, unavailableMsg } = this.config;
+        const { maps, statusMsg, unavailableMsg, timeout_ms } = this.config;
+        const timeout = Number(timeout_ms);
+
         try {
-            const info = await this.server.getInfo();
+            const info = await queryGameServerInfo(this.host, 1, timeout);
             const map = getMap(info.map, maps);
-            this.online = info.players.online;
+            this.online = info.players;
             this.bot.user.setPresence({
                 activities: [{
                     type: ActivityType.Playing,
-                    name: statusMsg.replaceAll('{online}', info.players.online).replaceAll('{max}', info.players.max).replaceAll('{map}', map.name)
+                    name: statusMsg.replaceAll('{online}', info.players).replaceAll('{max}', info.maxPlayers).replaceAll('{map}', map.name)
                 }],
                 status: 'online'
             });
 
             if (Object.keys(this.message).length === 0) return;
 
-            const players = await this.server.getPlayers();
+            const { players } = await queryGameServerPlayer(this.host, 1, timeout);
+
             const msgObject = createServMsg(info, players, map, this.bot.user.avatarURL(), this.config, this.serverData.buttons);
             this.message.edit(msgObject);
-
         }
-        catch (e) {
+        catch {
+            console.error(`Error.${this.host}: wrong server host or off`);
             this.bot.user.setPresence({ activities: [{ type: ActivityType.Watching, name: unavailableMsg }], status: 'dnd' });
-            throw new Error(e);
+
         }
-
-
     }
 }
 
@@ -157,7 +153,7 @@ function createButtons(buttons) {
     for (const button of buttons) {
         if (!button.url.startsWith('http')) continue;
         const btn = new ButtonBuilder().setStyle(ButtonStyle.Link).setLabel(button.label).setURL(button.url);
-        if(button.emoji) btn.setEmoji(button.emoji);
+        if (button.emoji) btn.setEmoji(button.emoji);
         buttonsArr.push(btn);
     }
 
